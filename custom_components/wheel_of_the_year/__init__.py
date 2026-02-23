3a753d91bbf4823ee1fcf955142517ed37c7236a
MorningstarOwl <ms.grace.schumann@gmail.com>
2026-02-23 13:50:23 -0700

    Create __init__.py

---
+++ custom_components/wheel_of_the_year/__init__.py
@@ -0,0 +1,20 @@
+"""The Wheel of the Year integration."""
+
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN, PLATFORMS
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Wheel of the Year from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
